ed0bcdc5db7be57978d1d001d2511aef3e6134fa
guilhermeivo <guilhermeivob@gmail.com>
2025-09-22 00:03:53 -0300

    resolvendo o build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@ set -e
 
 tlmgr info multibib > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-
+    echo "multibib já instalado"
 else
     tlmgr install multibib
 fi
 
 FILE=${1:-"abntlatex"}
 
+echo ">>> Rodando latex abntlatex.ins"
 latex abntlatex.ins
 
 pdflatex $FILE.dtx
@@ -21,5 +22,6 @@ pdflatex $FILE.dtx
 rm -f examples/canonical-model/abntlatex.cls
 cp abntlatex.cls examples/canonical-model
 
+echo ">>> Chamando build.sh em examples"
 cd examples
 ./build.sh --dir=canonical-model --file=model --mode=bib --simplify
