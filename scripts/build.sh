0c90e30bb1d5ecd4f533a6cdaef85f8ab2d337c7
Guilherme Ivo <guilhermeivob@gmail.com>
2026-06-16 22:58:09 -0300

    feat: scripts: add installation of the LaTeX class to the local TEXMF folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -ex
 
+ROOT_FOLDER="${PWD}"
 FILE=${1:-"abntlatex"}
+
+if [ ! -d "$ROOT_FOLDER/scripts" ]; then
+  echo "$ROOT_FOLDER/scripts does not exist."
+fi
 
 echo ">>> Rodando latex abntlatex.ins"
 latex abntlatex.ins
@@ -12,8 +17,13 @@ makeindex -s gglo.ist -o $FILE.gls $FILE.glo
 pdflatex $FILE.dtx
 pdflatex $FILE.dtx
 
-rm -f examples/canonical-model/abntlatex.cls
-cp abntlatex.cls examples/canonical-model
+TEXMF=$(kpsewhich -var-value TEXMFHOME)
+mkdir -p $TEXMF/tex/latex/abntlatex/
+mkdir -p $TEXMF/bibtex/bst/
+
+cp $ROOT_FOLDER/abntlatex.cls $TEXMF/tex/latex/abntlatex/
+cp $ROOT_FOLDER/abnt.bst $TEXMF/bibtex/bst/
+texhash
 
 echo ">>> Chamando build.sh em examples"
 cd examples
